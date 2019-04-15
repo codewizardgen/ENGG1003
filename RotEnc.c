cca0016a7b0bbc63eb9e43b1d68c53a30adf9fce
Genna Rowe <rowegenna@gmail.com>
2019-04-15 07:12:21 +0000

    going home for dinner

--- RotEnc.c
+++ RotEnc.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 
 char RotEnc (char x);
+char RotDec (char x);
 
 int main(void)
 {
     int index = 0, max = 100;
-    char message[] = "THIS IS AN EXAMPLE- SEND HELP!";  
+    char message[] = "YMNX NX FS JCFRUQJ XJSI MJQU";
+    //char message[] = "THIS IS AN EXAMPLE- SEND HELP!";  
     //Leaving the number of array elements blank
     //The compiler will assign enough memory for the elements there
     for(index = 0; index < max; index++){
         if (message[index] >= 65 && message[index]<=90){
             int temp = message[index]-65;
-    	    int encryption = RotEnc (temp);
-    	    printf("%d ", encryption);
+    	    //int encryption = RotEnc (temp);
+    	    //printf("%c ", encryption);
+    	    int decryption = RotDec (temp);
+    	    printf("%c", decryption);
         }
 	    else {
 	        printf(" ");
@@ -24,7 +28,12 @@ int main(void)
 char RotEnc (char x) {     
     char key = 5;
     x = (x + key)%26;
-    //if (x >= 26) {            
-    //    x-=26;
-    //}
+    x += 65;                        //add 65 so that numbers correctly align with correct section of ASCII
+    return x;
+}
+
+char RotDec (char x){
+    char key = 5;
+    x = (x - key)%26;
+    return x;
 }
