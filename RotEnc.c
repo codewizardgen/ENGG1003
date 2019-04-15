48d93428be7ad05c19bde6adcac6501623bcc2a8
Genna Rowe <rowegenna@gmail.com>
2019-04-15 06:44:11 +0000

    simple rotation

--- RotEnc.c
+++ RotEnc.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+char RotEnc (char x);
+
 int main(void)
 {
     int index = 0, max = 100;
@@ -7,11 +9,21 @@ int main(void)
     //Leaving the number of array elements blank
     //The compiler will assign enough memory for the elements there
     for(index = 0; index < max; index++){
-	    int temp = message[index]-65;
-	    printf("%d\n", temp);
+        if (message[index] >= 65 && message[index]<=90){
+            int temp = message[index]-65;
+    	    int encryption = RotEnc (temp);
+    	    printf("%d ", encryption);
+        }
+	    else {
+	        printf(" ");
+	    }
 	}
 	return 0;
-    
-    
-    return 0;
+}
+
+char RotEnc (char x) {
+    x = x + 5;
+    if (x >= 26) {
+        x-=26;
+    }
 }
