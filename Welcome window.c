db834cae83dd7a34d6001b3e5eec8d0221aae026
Genna Rowe <rowegenna@gmail.com>
2019-04-27 06:37:23 +0000

    switch outside main but default not printing to console

--- Welcome window.c
+++ Welcome window.c
@@ -9,6 +9,7 @@
 	The function prototypes are placed before main ().
 	*/
 	int welcome (void);
+	
 	char roundWeGo();
 	char goGoSubstitution();
 	char turnBackTime();
@@ -16,32 +17,9 @@
 
 int main(){
 	
-	int selection = welcome ();
-	switch (selection){
-    case 1:	printf("\nAlright! Rotation Encryption, Here's your encrypted text:\n");
-    		return roundWeGo ();				//Can a return value be a function?? Pretty sure. Do I want this. Just get skeleton done. 
-    		break;
-    
-    case 2: printf("\nSubstitution Encryption, right on. \nYour text has been encrypted:\n");
-    		return goGoSubstitution();			//Is the input type void? No. Because it needs to be passed our string of elements.
-    		break;
-    case 3: printf("\nSolving...\n				Rotation Decryption yeilded:\n");
-    		return turnBackTime ();				//The key always has to be the same the way i was thinking about it...
-    		break;									//Should there be a loop that shifts through rotation posibilities? Probably.
-    													//How many different rotations are possible? Consider A, if we normalise the values so that A is 0
-    													//25 other options. Implement an array as a condition in a loop. 
-    													//Increment sequentially similar to examples seen before.
-    case 4: printf("\nYou selected substitution decryption. \nA wizard has opened your message:\n");
-    		return alohamora();					//This is going to be a substisution decription but haven't come up with how best to go about it yet.
-    		break;								//Break statements mean the switch will exit once one of the cases has been executed.
-    	
-    default: printf("The action you chose (%d) isn't valid\n", selection);	
-        	}	
-
-	
-	
+	welcome ();
 }
-
+	
 /*
  FUNCTION DEFINITIONS contain the operational code of the function
  SYNTAX: 
@@ -67,14 +45,31 @@ int main(){
     	printf("What shall it be?	");
     	
     	scanf("%d", &s);
+    	
+    	switch (s){
+            case 1:	printf("\nAlright! Rotation Encryption, Here's your encrypted text:\n");
+            		return roundWeGo ();				//Can a return value be a function?? Pretty sure. Do I want this. Just get skeleton done. 
+            		break;
+            
+            case 2: printf("\nSubstitution Encryption, right on. \nYour text has been encrypted:\n");
+            		return goGoSubstitution();			//Is the input type void? No. Because it needs to be passed our string of elements.
+            		break;
+            case 3: printf("\nSolving...\n				Rotation Decryption yeilded:\n");
+            		return turnBackTime ();				//The key always has to be the same the way i was thinking about it...
+            		break;									//Should there be a loop that shifts through rotation posibilities? Probably.
+            													//How many different rotations are possible? Consider A, if we normalise the values so that A is 0
+            													//25 other options. Implement an array as a condition in a loop. 
+            													//Increment sequentially similar to examples seen before.
+            case 4: printf("\nYou selected substitution decryption. \nA wizard has opened your message:\n");
+            		return alohamora();					//This is going to be a substisution decription but haven't come up with how best to go about it yet.
+            		break;								//Break statements mean the switch will exit once one of the cases has been executed.
+            	
+            default: printf("The action you chose (%d) isn't valid\n", s);
+        	}	
 	   }
 	   while (s < 1 || s > 4);
    }
 
-
-    							//NOTE TO SELF: Replace stupid names before due. 
-    							//Prob should just use proper from start but eh.
-    							//Should be informative and easily interpreted by the reader
 
 
 char roundWeGo(){
