0b972a3a7f84ce20d739fdb8882561c0bceffbe4
Genna Rowe <rowegenna@gmail.com>
2019-04-28 03:39:22 +0000

    Two ways to print a string

--- headache.c
+++ headache.c
@@ -25,15 +25,11 @@ int main(){
     }
     printf("\n");
     
-    //what if I just want to print the first word
     //use %s to read a string
     //NOTE: no * for %s or error: expecting char* but it's an int
 
-        for(i=0; i<30; i++){
-            printf("%s\n", (str + i));
-            //NOTE: printf("%s", str[i]) is the same statement
-        }
- 
+    printf("%s\n", str);
+            
     return 0;
 }
 
