23b7c4593e8d4c6b7a286d952e1444377fb468cc
Genna Rowe <rowegenna@gmail.com>
2019-04-28 09:00:27 +0000

    Hard code encryption to function hard code

--- projectX.c
+++ projectX.c
@@ -1,20 +1,48 @@
 #include <stdio.h>
 
-
+void rotEnc(void);
 
 int main(){
     
+    ////////////////////////
+    //hard coded inputs 
+    ////////////////////////
     char text[30]= "PURPLE MICROWAVE DINASOUR";
     int k = 3;
     
-    //print the array by cycling through the characters
+    //call an input function here
+    //not used currently
+    
+    
+    
+    //////////////////////////
+    // encode
+    ///////////////////////////
     
     int i;
+
+    //hardcoded inputs
     int MAX = 30;
+    
+    //passes inputs;
+    
+    
+    //hard coded input based switch 
+    //encode_type=1; //1=rotary, 2=substitution
+    int encode_type = 1;
+    if (encode_type==1){
+        rotEnc();    
+    }
+    else if (encode_type==2){
+        //otherEnc);
+    }
+    
+    
+    
     for(i=0; i<MAX; i++){
         printf("%c", text[i]);
     }
-    printf("\n");
+    printf("a \n");
 
     //Rotation encryption
     int x, m;
@@ -28,5 +56,26 @@ int main(){
         x += 65;
         printf("%c", x);
     }
-    printf("\n");
+    printf("b \n");
+}
+
+void rotEnc(void){
+    //input variables - m for message, k for key
+    
+
+    char text[30]= "PURPLE MICROWAVE DINASOUR";
+    int MAX = 30;
+    int k = 3;
+    int x, m, i;
+    for (i=0; i<MAX;i++){
+        m = text[i] - 65;
+        if (text[i]>=65 && text[i]<=90){
+            x = (m + k)%26;
+        }
+        else
+            x = m;
+        x += 65;
+        printf("%c", x);
+    }
+    printf("c\n");
 }
