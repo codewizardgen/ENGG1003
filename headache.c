046c893738068cc0de7177c6483d1425ec68a694
Genna Rowe <rowegenna@gmail.com>
2019-04-28 05:05:34 +0000

    function practice following Sandeep Soni

--- headache.c
+++ headache.c
@@ -8,12 +8,19 @@
 //Each function performs a specific task and can return a value (only one)
 
 int sum();          //the function definition is known as forward declaration
+int greatest(int a, int b, int c);
 
 int main(){
 
-    int r1 = sum();
-    int r2 = sum();
-    printf("Result 1: %d\nResult 2 %d\n", r1, r2);
+    //int r1 = sum();
+    //int r2 = sum();
+    //printf("Result 1: %d\nResult 2 %d\n", r1, r2);
+    int n1, n2, n3;
+    n1 = 45;
+    n1 = 72;
+    n3 = 56;
+    int r1 = greatest(n1, n2, n3);
+    printf("%d is greatest", r1);
 }
 
 int sum(){
@@ -27,4 +34,18 @@ int sum(){
     //printf("Sum of %d, %d & %d is %d\n", a, b, c, a+b+c);
     return a+b+c;
 }
-    
+
+//NOTE: the function parameters (a, b, c) 
+//DO NOT have to be the same as the variables which are passed (n1, n2, n3)
+int greatest(int a, int b, int c){
+    if (a>b)
+        if (a>c)
+            return a;
+        else
+            return c;
+    else
+        if (b>c)
+            return b;
+        else
+            return c;        
+}
