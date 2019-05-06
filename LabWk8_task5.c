aea0babe8be84b6fb60de8cbb663c6a5b102e7c2
Genna Rowe <rowegenna@gmail.com>
2019-05-06 11:17:13 +0000

    Task 5 complete!

--- LabWk8_task5.c
+++ LabWk8_task5.c
@@ -15,6 +15,7 @@ int main(void) {
             fscanf(input, "%d\n", &arrayData[row][column]);
         } 
     }
+    //print the array as seen in the file
     for (row = 0; row <3; row++){
         for (column = 0; column <3; column++){
             printf("%d ", arrayData[row][column]);
@@ -26,4 +27,16 @@ int main(void) {
     
 
     // Find the largest value, print it, and it’s indices
+    int bigBoy = 0, rowMrkr = 0, colMrkr = 0;
+    for (row = 0; row < 3; row++){
+        for (column = 0; column <3 ; column++){
+            if (arrayData[row][column]>bigBoy){
+                bigBoy = arrayData[row][column];
+                rowMrkr = row;
+                colMrkr = column;
+            }
+        }
+    }
+    printf("Largest value: %d\n", bigBoy);
+    printf("Location in array: row = %d, column = %d\n", rowMrkr, colMrkr);
 }
